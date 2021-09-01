f77c58694d6c674938c2a620635c55c2df91e378
alperenkarate <alperensozen96@gmail.com>
2021-09-01 23:24:38 +0300

    used custom UI components with parameters and module styling for secondApp.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
 
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
-
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+import FirstApp from './components/FirstApp';
+import SecondApp from './components/SecondApp';
 
 const App = () => {
 
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  const addExpenseHandler = expense => {
-    setExpenses(prevExpenses => {
-      return [expense, ...prevExpenses];
-    });
-  }
-
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+    <div className="app">
+      <NavLink exact activeClassName="active-link" to="/">
+        <li className="nav-item">
+          <span className="nav-link" >First App</span>
+        </li>
+      </NavLink>
+      <NavLink exact activeClassName="active-link" to="/secondapp">
+        <li className="nav-item">
+          <span className="nav-link" >Second App</span>
+        </li>
+      </NavLink>
+
+      <Switch>
+        <Route path="/secondapp" render={() => <SecondApp />}></Route>
+        <Route path="/" render={() => <FirstApp />}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/SecondApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddUser from './Users/AddUser'
+
+function SecondApp() {
+    return (
+        <div>
+            <AddUser/>
+        </div>
+    )
+}
+
+export default SecondApp
